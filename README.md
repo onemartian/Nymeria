16ba68735cc9d1e5372a139a2eac5ddd374d31f4
onemartian <arunkokkula51@gmail.com>
2017-08-30 15:40:32 -0400

    initial release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nymeria
+Simple and fun an ester egg for node.
+
+Try it!
+```
+mkdir /tmp/test-repl
+cd /tmp/test-repl
+npm install nymeria
+>node
+>var nymeria = require('nymeria');
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+    console.log(
+      " Sourced from tehe Internet for javascript "+
+    "    "   + '\n' +
+"Don't Overlook Binding  "   + '\n' +
+  "Bind methods to objects  "   + '\n' +
+    "Rely on event delegation   "   + '\n' +
+      "Know when to use event delegation   "   + '\n' +
+        "Make AJAX responses cacheable  "   + '\n' +
+          "Put scripts at the bottom of the page   "   + '\n' +
+            "A class should only have one job  "   + '\n' +
+              "Objects or entities should be open for extension, but closed for modification  "   + '\n' +
+                "Foloow Liskov substitution principle  "   + '\n' +
+                  "Follow Interface segregation principle  "   + '\n' +
+                    "Follow Dependency inversion principle  "   + '\n' +                      "    "   + '\n' +
+                      "It always takes longer than you expect, even when you take into account Hofstadter's Law.   — Hofstadter's Law  "   + '\n' +
+                        "    "   + '\n' +
+                                  "    "   + '\n'
+                );
+})();
